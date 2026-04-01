6ea11d98b0db1f56bec13234c21c00a7ae5b66fa
saraswatchetan90-jpg <saraswatchetan90@gmail.com>
2026-04-01 15:18:15 +0530

    Time: 8 ms (3.77%) | Memory: 64.3 MB (6.24%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+       ArrayList<Integer> list=new ArrayList<>();
+       for(int i=0;i<nums.length;i++){
+        list.add(nums[i]);
+       }
+       Collections.rotate(list,k);
+       for(int i=0;i<nums.length;i++){
+        nums[i]=list.get(i);
+       }
+        }
+      
+    }
