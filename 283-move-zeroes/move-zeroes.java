ec1d39437708cd9cd219c9a1e7dd36a49e04c947
saraswatchetan90-jpg <saraswatchetan90@gmail.com>
2026-04-05 17:15:55 +0530

    Time: 2 ms (91.01%) | Memory: 47.6 MB (82.00%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+      int k=0;
+      for(int i=0;i<n;i++){
+        if(nums[i]!=0){
+            nums[k++]=nums[i];
+        }
+      }
+      while(k<n){
+        nums[k++]=0;
+      }
+     
+    }
+     
+}
