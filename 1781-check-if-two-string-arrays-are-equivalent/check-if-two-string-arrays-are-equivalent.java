ba625257f936b8c44f31e9f824b54fa4f474f533
saraswatchetan90-jpg <saraswatchetan90@gmail.com>
2026-04-06 22:20:10 +0530

    Time: 1 ms (91.27%) | Memory: 43.3 MB (47.79%) - LeetSync

---
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+             StringBuilder sb=new StringBuilder();
+	StringBuilder sb1=new StringBuilder();
+	
+	for(int i=0;i<word1.length;i++) {
+		sb.append(word1[i]);
+	}
+	for(int i=0;i<word2.length;i++) {
+		sb1.append(word2[i]);
+	}
+	if(sb.toString().equals(sb1.toString())) {
+		return true;
+	}
+	else {
+		return false;
+	}
+	 
+    }}
