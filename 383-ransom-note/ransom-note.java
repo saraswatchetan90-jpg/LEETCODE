03fe76feac4c4c7ecf7657a9f5ca4025c71d2890
saraswatchetan90-jpg <saraswatchetan90@gmail.com>
2026-04-16 19:18:28 +0530

    Time: 1 ms (99.93%) | Memory: 46.5 MB (54.43%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+       int count[]=new int[26];
+        for(int i=0;i<magazine.length();i++){
+            char ch=magazine.charAt(i);
+            count[ch-'a']++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            char ch=ransomNote.charAt(i);
+            if(count[ch-'a']==0){
+                return false;
+            }
+           count[ch-'a']--;
+        }
+        return true; 
+    }
+}
